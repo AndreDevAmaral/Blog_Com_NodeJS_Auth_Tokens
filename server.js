333d09e531cf2070334b65834a3fe9db729c6029
AndreDevAmaral <andre.dev.amaral@gmail.com>
2022-04-26 22:24:02 -0300

    Projeto Final

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
+
 const app = require('./app');
 const port = 3000;
 const db = require('./database');
+require('./redis/blacklist');
 
 const routes = require('./rotas');
 routes(app);
 
-app.listen(port, () => console.log(`App listening on port ${port}`));
+app.listen(port, () => console.log(`App listening on port http:localhost:${port}`));
